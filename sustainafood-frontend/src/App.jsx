f1243a94c028ae570854bfdca13763183e346217
FouzriChayma <chayma.fouzri@esprit.tn>
2025-02-17 22:33:50 +0100

    Added login page design

--- sustainafood-frontend/src/App.jsx
+++ sustainafood-frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './pages/Login2.jsx'
 import Log from './pages/log.jsx'
 
 function App() {

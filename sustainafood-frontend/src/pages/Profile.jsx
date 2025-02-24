486b31fe8495d9cfbaf39b1cf817e543ef31a029
mouna-mbr <mouna.benrebah@esprit.tn>
2025-02-24 21:31:29 +0100

    fixing all errors in profile and nav bar signup and signin

--- sustainafood-frontend/src/pages/Profile.jsx
+++ sustainafood-frontend/src/pages/Profile.jsx
@@ -60,7 +60,7 @@ const Profile = () => {
         <div className="main">
           <div className="left-column">
             <div className="profile-card">
-              <div className="card">
+              <div className="card-white ">
                 <button className="mail">
                   <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
                     <rect width={20} height={16} x={2} y={4} rx={2} />

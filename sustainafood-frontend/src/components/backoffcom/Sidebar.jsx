e04fa9f71d5eb9c0b807b6320fab8ece8646b523
mouna-mbr <mouna.benrebah@esprit.tn>
2025-04-03 02:13:22 +0100

    fixing root

--- sustainafood-frontend/src/components/backoffcom/Sidebar.jsx
+++ sustainafood-frontend/src/components/backoffcom/Sidebar.jsx
@@ -101,7 +101,7 @@ const Sidebar = () => {
               <Link to="/Donations">Donation Management</Link>
               <Link to="/food-donation/requests" className={location.pathname === "/food-donation/requests" ? "active" : ""} role="menuitem">Request Management</Link>
                <Link to="/PredictionsDashboard">Donation Predictions Dashboard</Link>
-               <Link to="/AnomaliesDashboard">Anomaly Management Dashboard</Link>
+               <Link to="/AnomaliesDashbord">Anomaly Management Dashboard</Link>
 
              {/* <Link to="/food-donation/product" className={location.pathname === "/food-donation/product" ? "active" : ""} role="menuitem">Product list</Link>*/}
             </div>
